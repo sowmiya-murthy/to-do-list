ffd0ffe1d67939260062af7f2260ef9f1f521e1c
sowmiya-murthy <linkwithsowmi@gmail.com>
2020-10-16 17:19:05 +0530

    Updates

---
+++ precache-manifest.20a7794322dca3a8467e70a29208a5ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bd93a50df5000096b7b3af1d1832db9",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "97428c3273d7100bde77",
+    "url": "/to-do-list/static/js/2.eea03e52.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/to-do-list/static/js/2.eea03e52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01edf3aa55522b9d2fd2",
+    "url": "/to-do-list/static/js/main.be2719eb.chunk.js"
+  },
+  {
+    "revision": "99620f4632993b275b31",
+    "url": "/to-do-list/static/js/runtime-main.65d94b1a.js"
+  }
+]);
